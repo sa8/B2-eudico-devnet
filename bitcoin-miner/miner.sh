b23167e24ad451bd45c5efde0cc0896175cc1d4a
Ubuntu <ubuntu@ip-172-26-13-172.eu-central-1.compute.internal>
2022-03-10 12:19:03 +0000

    edits still not working

--- bitcoin-miner/miner.sh
+++ bitcoin-miner/miner.sh
@@ -2,29 +2,29 @@
 
 sleep 30
 
-curl -u satoshi:amiens -X POST \
-    btc-rpc:8332 \
+curl -u sarah:pikachutestnetB2 -X POST \
+    btc-rpc:18332 \
     -d "{\"jsonrpc\":\"2.0\",\"id\":\"0\",\"method\":\"createwallet\", \"params\": [\"wow\"]}" \
     -H 'Content-Type:application/json'
 
-ADDRESS=$(curl -u satoshi:amiens -X POST \
-    btc-rpc:8332 \
+ADDRESS=$(curl -u sarah:pikachutestnetB2 -X POST \
+    btc-rpc:18332 \
     -d "{\"jsonrpc\":\"2.0\",\"id\":\"0\",\"method\":\"getnewaddress\", \"params\": [\"wow\"]}" \
     -H 'Content-Type:application/json' | jq -r '.result')
 
 echo "$ADDRESS"
 
-curl -u satoshi:amiens -X POST \
-    btc-rpc:8332 \
+curl -u sarah:pikachutestnetB2 -X POST \
+    btc-rpc:18332 \
     -d "{\"jsonrpc\":\"2.0\",\"id\":\"0\",\"method\":\"generatetoaddress\", \"params\": [150, \"$ADDRESS\"]}" \
     -H 'Content-Type:application/json'
 
-curl -u satoshi:amiens -X POST \
-    btc-rpc:8332 \
+curl -u sarah:pikachutestnetB2 -X POST \
+    btc-rpc:18332 \
     -d "{\"jsonrpc\": \"1.0\", \"id\":\"wow\", \"method\": \"sendtoaddress\", \"params\": [\"bcrt1p4ffl08gtqmc00j3cdc8x9tnqy8u4c0lr8vryny8um605qd78w7vs90n7mx\", 50]}" \
     -H 'Content-Type:application/json'
 
-while sleep 1; do curl -u satoshi:amiens -X POST \
-    btc-rpc:8332 \
+while sleep 1; do curl -u sarah:pikachutestnetB2 -X POST \
+    btc-rpc:18332 \
     -d "{\"jsonrpc\":\"2.0\",\"id\":\"0\",\"method\":\"generatetoaddress\", \"params\": [1, \"$ADDRESS\"]}" \
     -H 'Content-Type:application/json'; done
