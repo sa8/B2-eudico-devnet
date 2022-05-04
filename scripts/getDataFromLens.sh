d5613023e446fae8a179eed21d87ed9abfd2fa56
Ubuntu <ubuntu@ip-172-26-13-172.eu-central-1.compute.internal>
2022-05-04 17:01:43 +0000

    minor edits

--- scripts/getDataFromLens.sh
+++ scripts/getDataFromLens.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for i in {1..5}
+for i in {0..21}
 do
-	kubectl cp default/eudico-node-$i:data.txt data/test-$i.txt
+	kubectl cp default/eudico-node-$i:data.txt data/5-test-$i.txt
 done
